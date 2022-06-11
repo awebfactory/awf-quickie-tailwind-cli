e083112c72c962ff95796734a2657983d32c784a
Victor Kane <victorkane@gmail.com>
2022-06-11 12:54:50 -0300

    simple express server for html file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-https://stackblitz.com/github/awebfactory/awf-quickie-tailwind-cli/tree/daisyui
+var express = require('express');
+var app = express();
+
+app.use(express.static('html'));
+
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
+});
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000!")
+});
